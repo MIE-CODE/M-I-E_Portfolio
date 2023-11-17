90dcc3101f4958bb9100d22e408245bf3f47a613
Meyangz <Kayfolayan614@gmail.com>
2023-11-17 17:29:47 +0100

    working on progect page

--- app/Projects/page.tsx
+++ app/Projects/page.tsx
@@ -17,9 +17,21 @@ const page = () => {
       GithubLink: "https://github.com/MIE-CODE",
       liveLink: "df",
     },
+    {
+      GithubLink: "https://github.com/MIE-CODE",
+      liveLink: "df",
+    },
+    {
+      GithubLink: "https://github.com/MIE-CODE",
+      liveLink: "df",
+    },
+    {
+      GithubLink: "https://github.com/MIE-CODE",
+      liveLink: "df",
+    },
   ];
   return (
-    <div className="w-Screen  bg-gray-900 h-[100vh] max-h-max ">
+    <div className="w-Screen  bg-gray-900   ">
       <Header />
       <div className="p-5 pb-0">
         <p className=" font-mono text-blue-200/50 text-4xl font-extrabold">
@@ -28,7 +40,7 @@ const page = () => {
         <p className="animate-trans-right font-manrope text-sky-100">
           (M’I’E) Projects
         </p>
-        <div className="grid  gap-10 justify-center items-center   md:grid-cols-2 lg:grid-cols-3 mt-10  text-4xl font-extrabold text-blue-700">
+        <div className="grid p-10  gap-10 justify-center items-center   md:grid-cols-2 lg:grid-cols-3 mt-10  text-4xl font-extrabold text-blue-700">
           {projects.map((project, index) => (
             <div key={index} className="flex justify-center">
               <div className="flex relative justify-center items-center gap-1 w-[250px] h-[300px] shadow-slate-500/50 rounded-lg shadow-md">
@@ -43,7 +55,7 @@ const page = () => {
                     href={project.liveLink}
                     className=" w-max h-max p-2 hover:bg-slate-50/5 flex justify-center items-center shadow-slate-500/50 rounded-lg shadow-md"
                   >
-                    Live
+                    Live-Link
                   </Link>
                 </div>
               </div>
